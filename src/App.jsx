c0a6198eca9799a12456c51217c3bfdd5f96b8b5
Sriya Cheruku <sriyacheruku@Sriyas-MacBook-Air.local>
2026-05-26 17:28:31 +0530

    updated changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <body>
+    <main className="container">
       <div className="card">
         <h1>Contact Us</h1>
 
@@ -8,58 +8,107 @@ function App() {
           {/* First Name + Last Name */}
           <div className="name-row">
             <div className="input-group">
-              <label>First Name<span>*</span></label>
-              <input type="text" />
+              <label htmlFor="firstName">
+                First Name <span>*</span>
+              </label>
+
+              <input
+                id="firstName"
+                type="text"
+                required
+              />
             </div>
 
             <div className="input-group">
-              <label>Last Name<span>*</span></label>
-              <input type="text" />
+              <label htmlFor="lastName">
+                Last Name <span>*</span>
+              </label>
+
+              <input
+                id="lastName"
+                type="text"
+                required
+              />
             </div>
           </div>
 
           {/* Email */}
           <div className="input-group">
-            <label>Email Address<span>*</span></label>
-            <input type="email" />
+            <label htmlFor="email">
+              Email Address <span>*</span>
+            </label>
+
+            <input
+              id="email"
+              type="email"
+              required
+            />
           </div>
 
           {/* Query Type */}
           <div className="input-group">
-            <label>Query Type<span>*</span></label>
+            <label>
+              Query Type <span>*</span>
+            </label>
 
             <div className="query-row">
-              <div className="query-box">
-                <input type="radio" name="query" />
-                <span>General Enquiry</span>
-              </div>
+              <label className="query-box" htmlFor="general">
+                <input
+                  id="general"
+                  type="radio"
+                  name="query"
+                  required
+                />
 
-              <div className="query-box">
-                <input type="radio" name="query" />
+                <span>General Enquiry</span>
+              </label>
+
+              <label className="query-box" htmlFor="support">
+                <input
+                  id="support"
+                  type="radio"
+                  name="query"
+                  required
+                />
+
                 <span>Support Request</span>
-              </div>
+              </label>
             </div>
           </div>
 
           {/* Message */}
           <div className="input-group">
-            <label>Message<span>*</span></label>
-            <textarea></textarea>
+            <label htmlFor="message">
+              Message <span>*</span>
+            </label>
+
+            <textarea
+              id="message"
+              required
+            ></textarea>
           </div>
 
           {/* Checkbox */}
-          <div className="checkbox-group">
-            <input type="checkbox" />
-            <label>
-              I consent to being contacted by the team<span>*</span>
-            </label>
-          </div>
+          <label className="checkbox-group" htmlFor="consent">
+            <input
+              id="consent"
+              type="checkbox"
+              required
+            />
+
+            <span>
+              I consent to being contacted by the team
+              <span>*</span>
+            </span>
+          </label>
 
           {/* Button */}
-          <button>Submit</button>
+          <button type="submit">
+            Submit
+          </button>
         </form>
       </div>
-    </body>
+    </main>
   );
 }
 
